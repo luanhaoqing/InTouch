0d0cd9c8ed9617c857d567de85632dbf32a33548
Haoqing Luan <hluan@andrew.cmu.edu>
2017-02-15 19:45:43 -0500

    basic test for network function of Oculus

---
+++ Assets/RandomPosition.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.transform.position = new Vector3(Random.Range(0,10),0, Random.Range(0, 10));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
