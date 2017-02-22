ea6fd3f31c6b7b9eb8ca9889d986843e8625e4e1
Haoqing Luan <hluan@andrew.cmu.edu>
2017-02-22 18:33:47 -0500

    Added start position

---
+++ Assets/PlayerNumber.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+public class PlayerNumber : NetworkBehaviour {
+    public int PlayerCount = 0;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnPlayerConnected(NetworkPlayer player)
+    {
+        PlayerCount++;
+    }
+
+}
