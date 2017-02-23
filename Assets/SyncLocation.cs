237d22549eb6acf59de4d3fc18350faaa0f15fb7
Haoqing Luan <hluan@andrew.cmu.edu>
2017-02-22 22:32:51 -0500

    test sync

---
+++ Assets/SyncLocation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class SyncLocation : NetworkBehaviour {
+
+    public GameObject anchor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        } else
+        {
+            this.transform.position = anchor.transform.position;
+        }
+    }
+}
