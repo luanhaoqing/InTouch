a2e91934f7564160e262feee2d30c561e7e67abd
Haoqing Luan <hluan@andrew.cmu.edu>
2017-02-23 21:38:57 -0500

    test turn

--- Assets/TurnCounter.cs
+++ Assets/TurnCounter.cs
@@ -2,19 +2,12 @@
 using System.Collections;
 using UnityEngine.Networking;
 public class TurnCounter : NetworkBehaviour {
-
+    [SyncVar]
     public int count = 0;
     public int observers = 0;
 	// Use this for initialization
 	void Start () {
-        if(isServer)
-        {
-            Debug.Log("00000000000");
-        }
-        if(isClient)
-        {
-            Debug.Log("11111111111");
-        }
+        count++;
     }
 	
 	// Update is called once per frame
