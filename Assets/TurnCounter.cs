7785792377589a4061a8eda1a6d1219a16e7abaa
Haoqing Luan <hluan@andrew.cmu.edu>
2017-02-23 21:00:57 -0500

    spawn position

--- Assets/TurnCounter.cs
+++ Assets/TurnCounter.cs
@@ -1,21 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
-public class TurnCounter : MonoBehaviour {
+public class TurnCounter : NetworkBehaviour {
 
     public int count = 0;
 	// Use this for initialization
 	void Start () {
-       
+       if(isClient)
+        {
+            Debug.Log("111111111111");
+        }
+       else if(isServer)
+        {
+            Debug.Log("000000000000");
+        }
       //  OwnID = netId;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        count = Network.connections.Length;
+        
 	}
-    private void OnPlayerConnected(NetworkPlayer player)
-    {
-        count++;
-    }
+    
 }
