7240fcfc9151a1607e4e4a09289eec326d0a6bdd
Haoqing Luan <hluan@andrew.cmu.edu>
2017-02-23 23:02:22 -0500

    test turn

--- Assets/CurrentPlayer.cs
+++ Assets/CurrentPlayer.cs
@@ -6,33 +6,55 @@ public class CurrentPlayer : NetworkBehaviour {
     public int CurrentPlayerID;
     public float counter = 0;
     private bool reverse;
+    public GameObject TEXT;
 	// Use this for initialization
 	void Start () {
-        CurrentPlayerID = 0;
+        CurrentPlayerID = 1;
+        if (isLocalPlayer)
+            Debug.Log("111111111");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (!isServer)
-            return;
-        if (!reverse)
-        {
-            counter += Time.deltaTime;
-            if (counter >= 10f)
-            {
-                CurrentPlayerID = 1;
-                reverse = true;
 
-            }
-        }
-        else
+    // Update is called once per frame
+    void Update()
+    {
+        if (isServer)
         {
-            counter -= Time.deltaTime;
-            if (counter <= 0f)
+            if (!reverse)
             {
-                CurrentPlayerID = 0;
-                reverse = false;
+                counter += Time.deltaTime;
+                if (counter >= 10f)
+                {
+                    CurrentPlayerID = 2;
+                    reverse = true;
+
+                }
+            }
+            else
+            {
+                counter -= Time.deltaTime;
+                if (counter <= 0f)
+                {
+                    CurrentPlayerID = 1;
+                    reverse = false;
+                }
             }
         }
+        if (isLocalPlayer)
+        {
+           
+            if(CurrentPlayerID==this.GetComponent<TurnCounter>().OwnId)
+            {
+                TEXT.SetActive(true);
+            }
+            else
+            {
+                TEXT.SetActive(false);
+            }
+        }
+
     }
+
+
+
+
 }
