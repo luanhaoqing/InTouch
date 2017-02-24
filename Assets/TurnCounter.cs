1ce78f5a3f859a95d6b60d20c9314e30c453a1d0
Haoqing Luan <hluan@andrew.cmu.edu>
2017-02-23 20:55:40 -0500

    test turn

--- Assets/TurnCounter.cs
+++ Assets/TurnCounter.cs
@@ -12,7 +12,7 @@ public class TurnCounter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        count = Network.connections.Length;
 	}
     private void OnPlayerConnected(NetworkPlayer player)
     {
