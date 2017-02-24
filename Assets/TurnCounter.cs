7d28117f384d4a04e8243d1068c3bbe8bd9bfa4a
Haoqing Luan <hluan@andrew.cmu.edu>
2017-02-23 20:38:32 -0500

    test turn

---
+++ Assets/TurnCounter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+public class TurnCounter : NetworkBehaviour {
+    public bool ClientTrun = false;
+    public int CurrentID=0;
+    public int OwnID;
+	// Use this for initialization
+	void Start () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        OwnID = Network.connections.Length;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	if(!isLocalPlayer)
+        {
+            return;
+        }
+
+
+
+	}
+}
