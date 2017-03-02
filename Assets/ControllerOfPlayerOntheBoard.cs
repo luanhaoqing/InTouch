b4bb228908efa36ed509b4a018dd3dc05aebdb85
Haoqing Luan <hluan@andrew.cmu.edu>
2017-03-02 15:07:38 -0500

    added codes for map generator

---
+++ Assets/ControllerOfPlayerOntheBoard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class ControllerOfPlayerOntheBoard : NetworkBehaviour {
+    public GameObject PlayerOnBoard;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+       // Debug.Log("TEST");
+        PlayerOnBoard.transform.Rotate(0, x, 0);
+        PlayerOnBoard.transform.Translate(0, 0, z);
+
+
+
+    }
+}
