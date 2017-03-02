2ebf5c551df8744eb96dba7dfcb734f7a4d50a8c
Haoqing Luan <hluan@andrew.cmu.edu>
2017-03-02 18:57:56 -0500

    Map generator

--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -7,23 +7,24 @@ public class TileManager : NetworkBehaviour {
     public  GameObject[] tiles;
 	// Use this for initialization
 	void Start () {
-        if (!isServer)
-            return;
-        for(int i=0;i<6;i++)
+     /*   if (isServer)
         {
-            for(int j=0;j<8;j++)
+            for (int i = 0; i < 6; i++)
             {
-                if (i == 0 && j == 0)
-                    continue;
-               // GameObject tmp = Instantiate(tile);
-               this.GetComponent<GenerateMap>().GenerateTile(new Vector3(0.26f + j * 0.2f, 0, -0.46f + i * 0.2f),0);
-                GameObject tmp = Instantiate(tile);
-                tmp.transform.position = new Vector3(0.26f + j * 0.2f, 0, -0.46f + i * 0.2f);
-                tmp.transform.parent = this.transform;
-                tmp.transform.parent = this.transform;
-                tiles[6*i+j] = tmp;
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i == 0 && j == 0)
+                        continue;
+                    // GameObject tmp = Instantiate(tile);
+                   // this.GetComponent<GenerateMap>().GenerateTile(new Vector3(0.26f + j * 0.2f, 0, -0.46f + i * 0.2f), 0);
+                    GameObject tmp = Instantiate(tile);
+                    tmp.transform.position = new Vector3(0.26f + j * 0.2f, 0, -0.46f + i * 0.2f);
+                    tmp.transform.parent = this.transform;
+                    tmp.transform.parent = this.transform;
+                    tiles[6 * i + j] = tmp;
+                }
             }
-        }
+        }*/
 	}
 	
 	// Update is called once per frame
