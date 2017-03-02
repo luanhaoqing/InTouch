4ed5eb96938527225a9f8bb16eced8b57e22ff42
Haoqing Luan <hluan@andrew.cmu.edu>
2017-03-02 13:27:54 -0500

    added codes for map generator

--- Assets/Scripts/CurrentPlayer.cs
+++ Assets/Scripts/CurrentPlayer.cs
@@ -9,6 +9,7 @@ public class CurrentPlayer : NetworkBehaviour {
     public GameObject TEXT;
     public GameObject Table;
     private int test=0;
+    private bool isCreate;
 	// Use this for initialization
 	void Start () {
         CurrentPlayerID = 1;
@@ -27,8 +28,8 @@ public class CurrentPlayer : NetworkBehaviour {
                 {
                     CurrentPlayerID = 2;
                     reverse = true;
-                    int ran = Random.Range(0, 3);
-                    Table.GetComponent<GenerateMap>().GenerateTile(new Vector3(test++, 0, 0), ran);
+                   
+                   
                 }
             }
             else
@@ -38,8 +39,8 @@ public class CurrentPlayer : NetworkBehaviour {
                 {
                     CurrentPlayerID = 1;
                     reverse = false;
-                    int ran = Random.Range(0, 3);
-                    Table.GetComponent<GenerateMap>().GenerateTile(new Vector3(test++, 0, 0), ran);
+                   
+                   
                 }
             }
         }
@@ -49,11 +50,16 @@ public class CurrentPlayer : NetworkBehaviour {
             if(CurrentPlayerID==this.GetComponent<TurnCounter>().OwnId)
             {
                 TEXT.SetActive(true);
-
-                
+                if (!isCreate)
+                {
+                    int ran = Random.Range(0, 3);
+                    Table.GetComponent<GenerateMap>().GenerateTile(new Vector3(test++, 0, 0), ran);
+                    isCreate = true;
+                }
             }
             else
             {
+                isCreate = false;
                 TEXT.SetActive(false);
             }
         }
