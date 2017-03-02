1c560683d379f4a3c02d0dd91bf076c1836c9bce
Haoqing Luan <hluan@andrew.cmu.edu>
2017-03-02 15:09:47 -0500

    added codes for map generator

--- Assets/ControllerOfPlayerOntheBoard.cs
+++ Assets/ControllerOfPlayerOntheBoard.cs
@@ -11,7 +11,8 @@ public class ControllerOfPlayerOntheBoard : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-      
+        if (!isLocalPlayer)
+            return;
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
        // Debug.Log("TEST");
