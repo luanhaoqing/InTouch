6f5aa112ed00744ce6f3f4c682ef23bbd2140a8a
ANDREW\jiajunl2 <liujj312@qq.com>
2017-03-08 16:52:16 -0500

    use itween to add movement to inventory panel

---
+++ Assets/inventory_float.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inventory_float : MonoBehaviour {
+    public float riseHeight;
+
+
+	// Use this for initialization
+	void Start () {
+        InventoryMoveUp();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void InventoryMoveUp()
+    {
+        iTween.MoveBy(gameObject, iTween.Hash("y", riseHeight, "space","world", "easeType", "easeInOutCubic", "loopType", "pingPong", "delay", 5));
+    }
+}
