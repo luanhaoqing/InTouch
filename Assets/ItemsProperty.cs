492ac1cd0376766a5119b466bb95ea560b7a47c1
Haoqing Luan <hluan@andrew.cmu.edu>
2017-03-08 21:17:18 -0500

    trade iteams

--- Assets/ItemsProperty.cs
+++ Assets/ItemsProperty.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class ItemsProperty : MonoBehaviour {
     public int Player_ID;
+    public bool trade;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,7 +11,11 @@ public class ItemsProperty : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	if(trade)
+        {
+            TradeItem();
+            trade = false;
+        }
 	}
     private void OnTriggerEnter(Collider other)
     {
@@ -22,7 +27,17 @@ public class ItemsProperty : MonoBehaviour {
     }
     public void TradeItem()
     {
-
+        if (Player_ID != GameObject.Find("TrunCounter").GetComponent<TurnCounter>().OwnId)
+            return;
+        GameObject[] Players = GameObject.FindGameObjectsWithTag("Player");
+        for(int i=0;i<2;i++)
+        {
+            if(Players[i].GetComponent<PlayerIDOnBoard>().PlayerIDOB!=Player_ID)
+            {
+                Player_ID = Players[i].GetComponent<PlayerIDOnBoard>().PlayerIDOB;
+                this.gameObject.transform.position = Players[i].transform.position;
+            }
+        }
     }
 
 }
