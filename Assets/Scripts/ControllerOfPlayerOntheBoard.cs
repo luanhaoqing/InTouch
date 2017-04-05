6ac6bf6fe878888d3596e6435893f01d1bd779ad
Haoqing Luan <hluan@andrew.cmu.edu>
2017-04-04 21:29:50 -0400

    modified control bugs

--- Assets/Scripts/ControllerOfPlayerOntheBoard.cs
+++ Assets/Scripts/ControllerOfPlayerOntheBoard.cs
@@ -56,9 +56,11 @@ public class ControllerOfPlayerOntheBoard : NetworkBehaviour {
     void Update () {
         if (!isLocalPlayer)
             return;
+        Debug.Log(TurnCounter.GetComponent<TurnCounter>().OwnId);
         if (TurnCounter.GetComponent<TurnCounter>().OwnId == 1)
         {
             counterDirection = true;
+           
         }
         else
             counterDirection = false;
