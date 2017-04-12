ce02b370a99528bf5eddec7d5bfb5a0d6777d82f
ANDREW\jiajunl2 <liujj312@qq.com>
2017-04-12 01:50:11 -0400

    event movement implemented

--- Assets/Scripts/EventPropoty.cs
+++ Assets/Scripts/EventPropoty.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class EventPropoty : MonoBehaviour {
     public GameObject turnCounter;
+    public GameObject EventAnimationManager;
+    public GameObject body;
 	// Use this for initialization
 	void Start () {
         turnCounter = GameObject.Find("TrunCounter");
-	}
+        EventAnimationManager = GameObject.FindGameObjectWithTag("EventAnimationManager");
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,6 +24,15 @@ public class EventPropoty : MonoBehaviour {
             if (turnCounter.GetComponent<CurrentPlayer>().RemainActionPoint!=3)
                 turnCounter.GetComponent<CurrentPlayer>().RemainActionPoint = 0;
             this.gameObject.SetActive(false);
+
+            //play the event sheet animation
+
+            //1.get the player's camera position
+            body = other.GetComponentInParent<SyncLocation>().Body.gameObject;
+            Transform targetPosition = body.transform;
+            //2.send the position to
+            EventAnimationManager.GetComponent<EventSheetMovement>().StartFlying(targetPosition);
+
         }
     }
 }
