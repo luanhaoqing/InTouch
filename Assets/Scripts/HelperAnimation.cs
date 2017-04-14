6219f6ba49e1bdecb6281007b46a2144bdce6972
ANDREW\jiajunl2 <liujj312@qq.com>
2017-04-13 23:48:46 -0400

    add one missing sound + talking helper implemented

--- Assets/Scripts/HelperAnimation.cs
+++ Assets/Scripts/HelperAnimation.cs
@@ -7,21 +7,32 @@ public class HelperAnimation : NetworkBehaviour {
     public GameObject turnCount;
     public GameObject helper;
     public GameObject countsShow;
+    float helperTalkSpeed = 0.05f;
+    public Texture[] helperTalkTextureArray;
 
     bool prompted;
     bool first_time = true;
 
-	// Use this for initialization
-	void Start () {
+    bool helperTalkActive = false;
+    float helperTalkCounter = 0;
+    Material helperBodyMaterial;
+    int helperTalkIndex = 0;
+    float helperTalkDuration = 0;
+    float thisTalkDuration = 0;
+
+
+    // Use this for initialization
+    void Start () {
         if (!isLocalPlayer)
             return;
-        {
-            turnCount = GameObject.Find("TrunCounter");
-        }
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        turnCount = GameObject.Find("TrunCounter");
+        helperBodyMaterial = helper.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material;
+        SetHelperTalkActive(true, 5);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
         if (!isLocalPlayer)
             return;
         {
@@ -42,7 +53,33 @@ public class HelperAnimation : NetworkBehaviour {
                 prompted = false;
             }
         }
-	}
+
+        // The helper talking animation
+        // check if switch is on
+        if (helperTalkActive)
+        {
+            helperTalkCounter += Time.deltaTime; // the usual time counter
+            thisTalkDuration += Time.deltaTime; // to set total talking animation time
+            
+            if (helperTalkCounter > helperTalkSpeed)
+            {
+                helperTalkCounter = 0;
+                if (helperTalkIndex < helperTalkTextureArray.Length)
+                {
+                    helperBodyMaterial.SetTexture("_MainTex", helperTalkTextureArray[helperTalkIndex]); // change the mouth texture to next in line
+                    helperTalkIndex++;
+                }
+                else
+                {
+                    helperTalkIndex = 0;
+                }
+            }
+            if (thisTalkDuration >= helperTalkDuration) // when cycle is over, turn it off.
+            {
+                SetHelperTalkActive(false, 0);
+            }
+        }
+    }
 
     void HelperPrompt()
     {
@@ -58,5 +95,15 @@ public class HelperAnimation : NetworkBehaviour {
         }
 
 
+    }
+
+    public void SetHelperTalkActive(bool boolean, float seconds)
+    {
+        helperTalkActive = boolean;
+        helperTalkDuration = seconds;
+        if (boolean == false) // when turning off, make it go back to closed mouth.
+        {
+            helperBodyMaterial.SetTexture("_MainTex", helperTalkTextureArray[9]);
+        }
     }
 }
