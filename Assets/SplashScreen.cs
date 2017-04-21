ba68f4d485e28b6aad3fca61086c39e49d8f0c91
ANDREW\jiajunl2 <liujj312@qq.com>
2017-04-21 17:49:35 -0400

    upload tutorial with opening

---
+++ Assets/SplashScreen.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+    public GameObject startPrompt;
+    public OverallManager overallManager;
+
+    GameObject Panel;
+    float counter;
+    float speed = 0.75f;
+    private bool controllerClickMapping;
+
+    bool BlinkingStart = true;
+    public bool ThumbClicked = false;
+
+    // Use this for initialization
+    void Start () {
+        Panel = startPrompt.transform.GetChild(0).gameObject;
+
+        controllerClickMapping = ((Input.GetKeyDown(KeyCode.Space)
+        || OVRInput.GetDown(OVRInput.Button.PrimaryThumbstick)
+        || OVRInput.GetDown(OVRInput.Button.SecondaryThumbstick)));
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (BlinkingStart) { 
+            counter += Time.deltaTime;
+            if ( counter> speed)
+            {
+                startPrompt.SetActive(false);
+            }
+            if (counter > speed * 2)
+            {
+                startPrompt.SetActive(true);
+                counter = 0;
+            }
+            return;
+
+        }
+
+        if (!ThumbClicked)
+        {
+            if (controllerClickMapping)
+            {
+                ThumbClicked = true;
+                BlinkingStart = false;
+            }
+        }
+
+        if (ThumbClicked)
+        {
+            StartCoroutine(AfterClickAnim());
+        }
+
+    }
+
+    IEnumerator AfterClickAnim()
+    {
+        iTween.FadeTo(Panel, 0, 1f);
+        int times = 0;
+        foreach  (int i in System.Linq.Enumerable.Range(0, 10))
+        {
+            startPrompt.SetActive(!startPrompt.activeInHierarchy);
+            yield return new WaitForSeconds(0.1f);
+            times ++;
+        }
+        overallManager.afterSplash();
+        Destroy(this);
+    }
+}
