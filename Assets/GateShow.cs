98234f8b56853e98fe6081e21db52526150718e7
Haoqing Luan <hluan@andrew.cmu.edu>
2017-04-23 21:43:06 -0400

    added collider to the first tile

--- Assets/GateShow.cs
+++ Assets/GateShow.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class GateShow : MonoBehaviour {
     public GameObject door;
     public int count=0;
+    public GameObject enddetecter;
 	// Use this for initialization
 	void Start () {
         door.SetActive(false);
+        
 	}
 	
 	// Update is called once per frame
@@ -14,6 +16,7 @@ public class GateShow : MonoBehaviour {
         if(count==2)
         {
             door.SetActive(true);
+            enddetecter.SetActive(true);
             //this.enabled = false;
         }
 	}
