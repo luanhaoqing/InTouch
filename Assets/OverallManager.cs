e7a0aaa85470ef3071fadb25db90e9dbaf2c6575
ANDREW\jiajunl2 <liujj312@qq.com>
2017-05-03 16:51:07 -0400

    F1 to skip tutorial

--- Assets/OverallManager.cs
+++ Assets/OverallManager.cs
@@ -29,7 +29,10 @@ public class OverallManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            skipScene();
+        }
 
         // used to skip scene;
         if (skipSceneTrigger)
